5b92057c2a3ed833e22b67d81abacdab7ad7a34b
Anne Saber <iarabejo3@gmail.com>
2022-04-04 21:07:44 -0500

    Consolidated code for Engineer test file. Passed final test.

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,29 +1,13 @@
 const Engineer = require('../lib/Engineer');
 
-describe("Engineer profile", () => {
-  test("create a Engineer object", () => {
-    const engineer = new Engineer("Inna", "2", "iarabejo3@gmail.com", "inna-arabejo");
-    
-    it("Returns the name of employee", () => {
-      expect(engineer.getName()).toEqual("Inna");
-    });
 
-    it("Returns the id of employee", () => {
-      expect(engineer.getId()).toEqual("2");
-    });
-
-    it("Returns the email of employee", () => {
-      expect(engineer.getEmail()).toEqual("iarabejo3@gmail.com");
-    });
-
-    it("Returns the github of employee", () => {
-      expect(engineer.getGitHub()).toEqual("inna-arabejo");
-    });
-
-    it("Returns the role of employee", () => {
-      expect(engineer.getRole()).toEqual("engineer");
-    });
-
-  });
+test("Returns the github of engineer", () => {
+  const engineer = new Engineer("Inna", "2", "iarabejo3@gmail.com", "inna-arabejo"); 
+  expect(engineer.getGitHub()).toEqual("inna-arabejo");
 });
 
+test("Returns the role of engineer", () => {
+  const returnVal = "Engineer";
+  const engineer = new Engineer("Inna", "2", "iarabejo3@gmail.com", "inna-arabejo");
+  expect(engineer.getRole()).toEqual(returnVal);
+});
