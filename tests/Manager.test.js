18c8abcaec58540a8853863319af997aad249524
Anne Saber <iarabejo3@gmail.com>
2022-04-04 21:04:02 -0500

    Consolidated code for Manager test file. Passed test.

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,29 +1,13 @@
 const Manager = require('../lib/Manager');
 
-describe("Manager profile", () => {
-  test("create a Manager object", () => {
-    const manager = new Manager("Kyle", "4", "kyle@mailto.com", "HH1");
-    
-    it("Returns the name of employee", () => {
-      expect(manager.getName()).toEqual("Kyle");
-    });
 
-    it("Returns the id of employee", () => {
-      expect(manager.getId()).toEqual("4");
-    });
-
-    it("Returns the email of employee", () => {
-      expect(manager.getEmail()).toEqual("kyle@mailto.com");
-    });
-
-    it("Returns the office number of employee", () => {
-      expect(manager.getOfficeNum()).toEqual("HH1");
-    });
-
-    it("Returns the role of employee", () => {
-      expect(manager.getRole()).toEqual("manager");
-    });
-
-  });
+test("Returns the office number of manager", () => {
+  const manager = new Manager("Kyle", "4", "kyle@mailto.com", "HH1");
+  expect(manager.getOfficeNum()).toEqual("HH1");
 });
 
+test("Returns the role of manager", () => {
+  const returnVal = "Manager";
+  const manager = new Manager("Kyle", "4", "kyle@mailto.com", "HH1");
+  expect(manager.getRole()).toEqual(returnVal);
+});
