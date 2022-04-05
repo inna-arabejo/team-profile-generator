69a90256c72c136bdf9a6942ce38202b4894a7aa
Anne Saber <iarabejo3@gmail.com>
2022-04-04 20:49:09 -0500

    Updating test in employee test file. 3 failed tests left to pass.

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,28 +1,34 @@
 const Employee = require('../lib/Employee');
 
-describe("Team", () => {
-  test("create an Employee object", () => {
-    const employee = new Employee("Carl", "1", "carl@mailto.com");
-    
-    it("Returns the name of employee", () => {
-      expect(employee.getName()).toEqual("Carl");
-    });
+// describe("Team", () => {
+// });
 
-    it("Returns the id of employee", () => {
-      expect(employee.getId()).toEqual("1");
-    });
 
-    it("Returns the email of employee", () => {
-      expect(employee.getEmail()).toEqual("carl@mailto.com");
-    });
-
-    it("Returns the role of employee", () => {
-      expect(employee.getRole()).toEqual("manager");
-    });
-
-  });
+test("create an Employee object", () => {
+  const employee = new Employee("Carl", "1", "carl@mailto.com");
+  expect(typeof(employee)).toEqual("object");
+});
+  
+test("Returns the name of employee", () => {
+  const employee = new Employee("Carl", "1", "carl@mailto.com");
+  expect(employee.getName()).toEqual("Carl");
 });
 
+test("Returns the id of employee", () => {
+  const employee = new Employee("Carl", "1", "carl@mailto.com");
+  expect(employee.getId()).toEqual("1");
+});
+
+test("Returns the email of employee", () => {
+  const employee = new Employee("Carl", "1", "carl@mailto.com");
+  expect(employee.getEmail()).toEqual("carl@mailto.com");
+});
+
+test("Returns the role of employee", () => {
+  const returnVal = "Employee";
+  const employee = new Employee("Carl", "1", "carl@mailto.com");
+  expect(employee.getRole()).toEqual(returnVal);
+});
 
 
 
